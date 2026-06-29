c14d8745404e26f5b8514e3a86c5623c227e150f
Jean MBODJ <jeansidymbodji@gmail.com>
2026-06-29 10:35:31 +0200

    Add monthly growth percentage calculation
    Added a Common Table Expression (CTE) to calculate monthly growth percentage based on total profit.

--- sql/queries.sql
+++ sql/queries.sql
@@ -5,7 +5,21 @@ FROM "Superstore"
 GROUP BY "Year_Month"
 ORDER BY "Year_Month";
 
+-- Monthly growth_percentage
+WITH "Monthly_Sales" AS 
+    (SELECT
+    EXTRACT(YEAR FROM "Order Date") AS YEAR,
+    EXTRACT(MONTH FROM "Order Date") AS MONTH,
+    SUM("Profit") as "Total_profit"
+    FROM "Superstore"
+    GROUP BY YEAR,MONTH
+    ORDER BY YEAR, MONTH)
 
+    SELECT*,
+    LAG("Total_profit") OVER (ORDER BY MONTH) AS "Previous_profit",
+    (("Total_profit" - LAG("Total_profit") OVER (ORDER BY MONTH))/
+    LAG("Total_profit") OVER (ORDER BY MONTH))*100 AS "Growth_percentage"
+    FROM "Monthly_Sales";
 
 -- Top 5 High Profits Products
 SELECT "Product Name",
